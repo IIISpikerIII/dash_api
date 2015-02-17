f2aac8900decf03817a7d3a6b623cfc9f3e510bb
spiker <iiispikeriii@gmail.com>
2015-02-17 23:05:26 +0300

    abstract class

--- src/Board.php
+++ src/Board.php
@@ -6,11 +6,14 @@
  * Time: 19:51
  */
 
-namespace boards;
+require_once(__DIR__.'/I_Board.php');
 
 abstract class Board implements I_Board {
 
-    protected $config = [];
+    protected $config = [
+        'method'    =>  'GET',
+        'auth'      =>  false,
+    ];
 
     /**
      * Board constructor
@@ -23,8 +26,48 @@ abstract class Board implements I_Board {
         }
     }
 
-    public function run($date_start, $date_stop){
+    /**
+     * Get url for request data
+     * @param $date_start
+     * @param $date_stop
+     * @return string
+     */
+    protected function requestUrl($date_start, $date_stop) {
 
+        return sprintf($this->config['link'], $date_start, $date_stop);
+    }
+
+    /**
+     * Get data from url
+     * @param $url
+     * @return mixed
+     */
+    protected function getData($url){
+
+        if( $curl = curl_init() ) {
+            curl_setopt($curl, CURLOPT_URL, $url);
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+            $out = curl_exec($curl);
+            curl_close($curl);
+            return $out;
+        }
+    }
+
+    protected function parseData($data) {
+
+        return json_decode($data);
+    }
+
+    public function run($date_start, $date_stop) {
+
+        $start = strtotime($date_start);
+        $stop = strtotime($date_stop);
+        $url = $this->requestUrl($start, $stop);
+
+        $data = $this->getData($url);
+        $data = $this->parseData($data);
+
+        return $data;
     }
 
 }
